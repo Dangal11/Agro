09bdacf1af0840ffaf21c0c71b1f89dacfff87dd
Dan Galindo <daniel-galindo17@outlook.com>
2024-01-24 10:14:11 -0600

    arquitectua de proyecto agro

---
+++ Agro.web/Data/Entities/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+    {
+        //Creado por DAGM
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
